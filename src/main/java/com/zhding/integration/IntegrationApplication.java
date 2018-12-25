1f0ffa6155f5e449953ab454bbabf012f3aecb9f
dzh1104 <zhding1104@163.com>
2018-12-25 11:23:05 +0800

    初始化工程 和 添加 Web HelloWorld

---
+++ src/main/java/com/zhding/integration/IntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.zhding.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntegrationApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/zhding/integration/web/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.zhding.integration.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController == @Controller + @ResponseBody
+@RestController
+public class HelloWorld {
+
+    // @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    @GetMapping("/helloworld")
+    public String sayHelloWorld() {
+        return "Hello World !";
+    }
+}
