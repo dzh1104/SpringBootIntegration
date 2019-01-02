ccc07a627de4ad1a19909c64bba4ac4807040f98
dzh1104 <zhding1104@163.com>
2019-01-02 11:27:45 +0800

    配置端口号 和 项目根路径

---
+++ src/test/java/com/zhding/integration/web/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.zhding.integration.web;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+@Test
+public void sayHelloWorld() {
+//	在java源代码的 类的名称 上面 option + enter 快速创建 ut
+
+}
+
+}
